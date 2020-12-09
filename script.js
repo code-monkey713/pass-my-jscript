099be7a046601c697e0960ab76bb1fc7bfaba371
Tony Tran <codemonkey713@gmail.com>
2020-12-08 23:17:44 -0600

    created java HTML to test logic code for JS for quicker testing

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-// declaring variables for global use with VAR
+// declaring variables values array for global use
 var lowerCase = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, 'm', `n`, `o`, `p`, 'q', `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
 var upperCase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, 'M', `N`, `O`, `P`, 'Q', `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var sChar = [` `, `!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `\``];
+
+// declaring variables with global use for simplicity in figuring out the code
 var lower;
 var upper;
 var number;
 var special;
-
-
+var length;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
-  var length = prompt(`Enter the desired password length from 8 to 128 characters.`);
+  
+  length = parseInt(prompt(`Enter the desired password length from 8 to 128 characters.`));
   if (length >= 8 && length <= 128) {
     alert( `You selected ` + length );
   } else {
